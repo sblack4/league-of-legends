925479858185b1ab061dda99660c0edb2bdac0d6
sblack4 <genseb7@gmail.com>
2018-10-31 14:30:16 -0400

    commiting before deleting

--- python/src/riot.py
+++ python/src/riot.py
@@ -1,5 +1,12 @@
 import json
 import requests
+from requests import Response
+
+
+class RateLimitException(Exception):
+    """basic exception """
+    pass
+
 
 class Riot:
     def __init__(self, api_key):
@@ -13,38 +20,64 @@ class Riot:
             "featuredMatches": "/lol/spectator/v3/featured-games"
         }
 
+    def checkStatus(self, response: Response):
+        status_code = response.status_code
+        if status_code == 429:
+            raise RateLimitException()
+
     def getUrl(self, endpoint, arg):
         return self.url_base + self.routes[endpoint] + "/" + str(arg) + self.api_arg
 
     def getMatchList(self, account_id):
         url = self.getUrl("matchlist", account_id)
-        return requests.get(url).json()
+        response = requests.get(url)
+        self.checkStatus(response)
+        return response.json()
 
     def getSummonerByName(self, name):
         url = self.getUrl("summoners/by-name", name)
-        return requests.get(url).json()
+        response = requests.get(url)
+        self.checkStatus(response)
+        return response.json()
 
     def getMatch(self, id):
         url = self.getUrl("matches", id)
-        response = requests.get(url)
+        response: Response = requests.get(url)
+        self.checkStatus(response)
         return response.json()
 
     def getFeaturedMatches(self):
         url = self.url_base + self.routes["featuredMatches"] + self.api_arg
         response = requests.get(url)
+        self.checkStatus(response)
         return response.json()
 
 
 if __name__ == "__main__":
-    key = "RGAPI-ac8d4e31-c857-4429-8615-db61b14cbb66"
+    key = "RGAPI-eb49c30b-3d29-4160-a19a-3ab744f48aa0"
     api = Riot(key)
 
-    seed_act_id =
+    players = [
+        'Faker',
+        'Westdoor',
+        'Marin',
+        'ssumday',
+        'huni',
+        'bjergsen',
+        'ziv',
+        'aphromoo',
+        'piccaboo',
+        'bang',
+        'doublelift',
+        'pawn',
+        'clearlove',
+        'yellowstar',
+        'deft',
+        'pyl',
+        'rookie',
+        'kakao',
+        'imp'
+    ]
+    for i in range(100):
+        print(api.getFeaturedMatches())
 
-    featured_matches = api.getMatchList(seed_act_id)
-    for match in featured_matches["gameList"]:
-        match_id = match['gameId']
-        match = api.getMatch(match_id)
-        fh = open(str(match_id) + ".json", 'w+')
-        fh.write(json.dumps(match, indent=2))
-        fh.close()
